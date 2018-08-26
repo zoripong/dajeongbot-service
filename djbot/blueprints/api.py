03eee24051e6667eb060be68fd29cca013cda455
zoripong <dbfl0406@e-mirim.hs.kr>
2018-08-26 20:10:22 +0900

    memory def complete

--- djbot/blueprints/api.py
+++ djbot/blueprints/api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint, request, jsonify
 
 from djbot.models.models import *
@@ -14,6 +16,9 @@ bp = Blueprint('api', __name__, url_prefix='/apis')
 
 @bp.route("/")
 def hello_world():
+    current = datetime.datetime.now()
+    tomorrow = current + datetime.timedelta(days=6)
+    print(tomorrow)
     return 'hello world'
 
 
@@ -175,16 +180,16 @@ def add_messages():
     db.session.add(chat)
     db.session.commit()
 
-    result = reply_message(content)
-
-    # print(result)
-    # result = {"status": "Success"}
+    # 챗봇이랑 대화 chat_type으로 분류
+    if content['chat_type'] == 0 :
+        result = reply_message(content)
+    elif content['chat_type'] == 1:
+        result = reply_message_for_memory(content)
 
     return result
 
 
 def add_message_for_new_user(account_id):
-
     ts = calendar.timegm(time.gmtime())
     # TODO : 캐릭터 컨셉으로 변환
     content = '반가워요 무엇을 도와드릴까요?' 
@@ -197,6 +202,8 @@ def add_message_for_new_user(account_id):
 
 # 챗봇이 답장을 주는 부분
 def reply_message(content):
+    current = datetime.datetime.now()
+    # 챗봇
     reply = danbee.message(content['content'], content['response'])
     reply_result = reply['responseSet']['result']['result']
 
@@ -213,33 +220,96 @@ def reply_message(content):
             and reply['responseSet']['result']['ref_intent_id'] == "":
         result = {
             "status": "Intent is not Found",
-            "intent": ""
         }
     else:
         # TODO : TEST
         # TODO : DEBUG : result is empty
-
         # 일정 등록
         # node_id가 SpeakNode_1533084803517에서 Event 저장
-        if reply['responseSet']['result']['ins_id'] == "SpeakNode_1533084803517":
+        if reply['responseSet']['result']['node_id'] == "SpeakNode_1533084803517":
             param = reply['responseSet']['result']['parameters']
             message_result = reply['responseSet']['result']['result']
-            event = Event(account_id=content['account_id'], schedule_when=param['when'], schedule_where=param['where'],
+            when = current + datetime.timedelta(days=param['where'])
+            event = Event(account_id=content['account_id'], schedule_when=param['when'], schedule_where=when,
                           schedule_what=param['what'], assign_time=message_result[0]['timestamp'],
                           detail=param['detail'])
             db.session.add(event)
             # TODO : FCM alarm 예약
 
-        elif reply['responseSet']['result']['ins_id'] == "SpeakNode_1533088132355":
+        elif reply['responseSet']['result']['node_id'] == "SpeakNode_1533088132355":
             # TODO: 지난 추억 가져오기
-            print("추억 회상 ! 뿅!")
+            num_date = reply['responseSet']['result']['parameters']['date']
+            if num_date < 0:
+                when = current + datetime.timedelta(days=num_date)
+                events = Event.query.filter(Event.account_id == content['account_id'], Event.schedule_when == when)\
+                    .order_by(Event.id)
+
+                json_events = jsonify([{
+                    "id": event.id,
+                    "schedule_when": event.schedule_when,
+                    "schedule_where": event.schedule_where,
+                    "schedule_what": event.schedule_what,
+                    "assign_time": event.assign_time,
+                    "detail": event.detail,
+                    "review": event.review
+                } for event in events])
+
+                if len(json_events) == 0:
+                    result = {
+                        "status": "Success",
+                        "node_type": 0,
+                        "message": ["그 날 알려준 이야기가 없네.. ㅠㅠ!", "혹시 다른 날이 아닐까?"],
+                        "events": json_events
+                    }
+                result = {
+                    "status": "Success",
+                    "node_type": 2,
+                    "message": ["일정들은 이렇게 돼!", "궁금한 날을 골라봐!"],
+                    "events": json_events
+                }
 
         result = reply
-
     db.session.commit()
     return jsonify(result)
 
-    # print(content['account_id'])
+
+# 추억회상 답장을 주는 로직
+def reply_message_for_memory(content):
+    # TODO : DB add
+    result = {
+        "status": "Failed",
+    }
+    max_index = len(content['response']['events'])
+    if max_index <= content['response']['select_idx']:
+        # 궁금한 일정이 없음
+        result = {
+            "status": "Success",
+            "node_type": 0,
+            "message": ["그래 또 궁금한 거 있으면 언제든지 물어봐!"],
+            "events": []
+        }
+    else:
+        select_idx = content['response']['select_idx']
+        event = content['response']['events'][select_idx]
+        message = []
+
+        schedule = event['schedule_where'] + "에서 " + event['schedule_what']+"했었구나!"
+        message.append(schedule)
+
+        if event['detail'] != 'null':
+            message.append("자세한 일정으로는 \""+event['detail']+"\"라고 말해줬어~")
+
+        if event['review'] != 'null':
+            message.append("그리고 그 일정을 다녀온 너는 \""+event['review']+"\"라고 나에게 이야기 해주었어!")
+
+        result = {
+            "status": "Success",
+            "node_type": 2,
+            "message": message,
+            "events": content['response']['events']
+        }
+
+    return jsonify(result)
 
 
 @bp.route('/messages/<int:res_account_id>')
@@ -276,7 +346,7 @@ def get_more_messages(res_account_id, last_index):
 # start of event code
 @bp.route('/events/<int:account_id>/<string:year>/<string:month>/<string:date>')
 def get_events(account_id, year, month, date):
-    date = year + "." + month + "." + date + "."
+    date = year + "-" + month + "-" + date
     js = {
         "status": "Failed"
     }

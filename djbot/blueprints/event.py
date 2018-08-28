c9e13bee88cc905af97595d2e02809cd3ea95f2b
zoripong <dbfl0406@e-mirim.hs.kr>
2018-08-28 14:24:08 +0900

    modify the response json

--- djbot/blueprints/event.py
+++ djbot/blueprints/event.py
@@ -3,18 +3,10 @@ from djbot.models.models import *
 
 bp = Blueprint('event', __name__, url_prefix='/events')
 
-@bp.route('/<int:account_id>')
-def get_events(account_id):
+@bp.route('/dates/<int:account_id>')
+def get_dates_having_event(account_id):
     events = Event.query.filter(Event.account_id == account_id).order_by(Event.id)
-    return jsonify([{
-        "id": event.id,
-        "schedule_when": event.schedule_when,
-        "schedule_where": event.schedule_where,
-        "schedule_what": event.schedule_what,
-        "assign_time": event.assign_time,
-        "detail": event.detail,
-        "review": event.review
-    }for event in events])
+    return jsonify([ event.schedule_when for event in events])
 
 
 @bp.route('/<int:account_id>/<string:year>/<string:month>/<string:date>')

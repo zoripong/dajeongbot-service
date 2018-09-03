b27004089552db771b45aed5aa53580552b38380
Han Yuri <dbfl0406@e-mirim.hs.kr>
2018-09-04 00:44:06 +0900

    register event debug

--- djbot/controllers/schedule.py
+++ djbot/controllers/schedule.py
@@ -15,13 +15,17 @@ from djbot.models.models import *
 push_service = FCMNotification(api_key=config.FCM_API)
 
 
-def register_event(reply, content):
+def register_event(reply, content, id):
     current = datetime.datetime.now()
     param = reply['responseSet']['result']['parameters']
     message_result = reply['responseSet']['result']['result']
     when = current + datetime.timedelta(days=int(param['when']))
     when = when.strftime('%Y-%m-%d')
-    event = Event(account_id=content['account_id'], schedule_when=when, schedule_where=param['where'],
+
+    print('여기보세요~')
+    print(content)
+
+    event = Event(account_id=id, schedule_when=when, schedule_where=param['where'],
                   schedule_what=param['what'], assign_time=message_result[0]['timestamp'],
                   detail=param['detail'])
     db.session.add(event)

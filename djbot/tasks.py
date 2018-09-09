739c76ca11c02a9b28bb626a1feffe67879b61b8
Han Yuri <dbfl0406@e-mirim.hs.kr>
2018-09-09 16:41:16 +0900

    Debug tasks.py message

--- djbot/tasks.py
+++ djbot/tasks.py
@@ -52,10 +52,8 @@ def register_calendar_notification():
 
         }
 
-        send_fcm_message(account['notify_time'], event['account_id'], 3, contents, param)
+        send_fcm_message(event, account['notify_time'], event['account_id'], 3, contents, param)
         # 해당 일정에 대해 안내 하였음을 업데이트 함
-        event.send = 1
-    db.session.commit()
 
 
 # 회원이 등록한 일정이 끝났을 때 일정에 대한 질문을 예약합니다.
@@ -78,13 +76,13 @@ def register_calendar_question():
             "data": data
         }
 
-        send_fcm_message(account['ask_time'], event['account_id'], 4, result_array, param)
+        send_fcm_message(event, account['ask_time'], event['account_id'], 4, result_array, param)
         # TODO : review update
 
 
 # 시간을 비교하여 사용자의 기기에 fcm 알림을 보냅니다.
 # param : 비교 시간, 계정 식별번호, 알림 제목, 알림 내용
-def send_fcm_message(check_time, account_id, chat_type, contents, param):
+def send_fcm_message(event, check_time, account_id, chat_type, contents, param):
 
     user_datetime_object = time.strptime(check_time, '%H:%M')
     user_time = datetime.time(user_datetime_object[3], user_datetime_object[4]).strftime("%H:%M")
@@ -95,9 +93,11 @@ def send_fcm_message(check_time, account_id, chat_type, contents, param):
             chat = Chat(account_id=account_id, content=content, node_type=0,
                         chat_type=chat_type, time=str(int(time.time() * 1000)), isBot=1)
             db.session.add(chat)
-        db.session.commit()
 
         tokens = FcmToken.query.filter(FcmToken.account_id == account_id).all
         for token in tokens:
             # send the fcm notification
             push_service.notify_single_device(registration_id=token['token'], data_message=param, content_available=True)
+
+        event.send = 1
+        db.session.commit()

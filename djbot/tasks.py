b67d7c88e70ee624ec3acadc0bb1dd69a808d87d
Han Yuri <dbfl0406@e-mirim.hs.kr>
2018-09-10 02:39:37 +0900

    seperate chat type and update db column

--- djbot/tasks.py
+++ djbot/tasks.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 import datetime
 import time
-import danbee
 
 from pyfcm import FCMNotification
 
@@ -49,11 +48,12 @@ def register_calendar_notification():
                     "content": contents
                 }
             }
-
         }
 
-        send_fcm_message(event, account['notify_time'], event['account_id'], 3, contents, param)
         # 해당 일정에 대해 안내 하였음을 업데이트 함
+        if send_fcm_message(event, account['notify_time'], event['account_id'], 3, contents, param):
+            event.notification_send = 1
+    db.session.commit()
 
 
 # 회원이 등록한 일정이 끝났을 때 일정에 대한 질문을 예약합니다.
@@ -61,28 +61,58 @@ def register_calendar_notification():
 def register_calendar_question():
     # 오늘 일어난 event 중 후기가 null 이면서 사용자의 일기쓰는 시간이 지난 경우
     today = datetime.datetime.now().strftime("%Y-%m-%d")
-    events = Event.query.filter(Event.review.is_(None), Event.schedule_when == today).order_by(Event.id).all()
 
-    for event in events:
-        account = Account.query.filter(Account.id == event['account_id'])
-
+    accounts = Account.query.all()
+    for account in accounts:
+        events = Event.query\
+            .filter(Event.review.is_(None), Event.schedule_when == today, Event.account_id == account['id'])\
+            .order_by(Event.id).all()
         # danbee.ai 에 request
-        data = danbee.message("일정 후기")
-        result_array = data['responseSet']['result']['result']
+        # data = danbee.message("일정 후기")
+        # result_array = data['responseSet']['result']['result']
+        event_json = []
+        for event in events:
+            event_json.append({
+                "id": event['id'],
+                "account_id": event['account_id'],
+                "schedule_when": event['schedule_when'],
+                "schedule_where": event['schedule_where'],
+                "schedule_what": event['schedule_what'],
+                "assign_time": event['assign_time'],
+                "detail": event['detail'],
+                "review": event['review'],
+                "notification_send": event['notification_send'],
+                "question_send": event['question_send']
+            })
 
+        content = ["오늘 하루 어땠니?"]
         param = {
             "title": "당신의 하루를 다정봇에게 들려주세요 :)",
-            "message": event['schedule_where'] + "에서 " + event['schedule_what'],
-            "data": data
+            "message": "오늘 "+str(len(events))+"개의 일정이 있습니다.",
+            "data": {
+                "status": "Success",
+                "result": {
+                    "id": account['id'],
+                    "node_type": 2,
+                    "chat_type": 4 ,
+                    "time": str(int(time.time() * 1000)),
+                    "img_url": [],
+                    "content": content,
+                    "events": event_json
+                }
+            }
         }
 
-        send_fcm_message(event, account['ask_time'], event['account_id'], 4, result_array, param)
-        # TODO : review update
+        # 해당 일정에 대해 안내 하였음을 업데이트 함
+        if send_fcm_message(account['ask_time'], account['id'], 4, content, param):
+            for event in events:
+                event.question_send = 1
+    db.session.commit()
 
 
 # 시간을 비교하여 사용자의 기기에 fcm 알림을 보냅니다.
 # param : 비교 시간, 계정 식별번호, 알림 제목, 알림 내용
-def send_fcm_message(event, check_time, account_id, chat_type, contents, param):
+def send_fcm_message(check_time, account_id, chat_type, contents, param):
 
     user_datetime_object = time.strptime(check_time, '%H:%M')
     user_time = datetime.time(user_datetime_object[3], user_datetime_object[4]).strftime("%H:%M")
@@ -93,11 +123,13 @@ def send_fcm_message(event, check_time, account_id, chat_type, contents, param):
             chat = Chat(account_id=account_id, content=content, node_type=0,
                         chat_type=chat_type, time=str(int(time.time() * 1000)), isBot=1)
             db.session.add(chat)
+            db.session.commit()
 
-        tokens = FcmToken.query.filter(FcmToken.account_id == account_id).all
+        tokens = FcmToken.query.filter(FcmToken.account_id == account_id).all()
         for token in tokens:
             # send the fcm notification
+            print("메세지 전송 얍!")
             push_service.notify_single_device(registration_id=token['token'], data_message=param, content_available=True)
 
-        event.send = 1
-        db.session.commit()
+        return True
+    return False
